378b285118272d1c701332d9c2152b84673a3577
demacedoleo <leonardo.demacedo@pedidosya.com>
2022-12-13 19:32:33 -0300

    add test default context loads

--- src/test/java/com/capitole/challenge/pricing/PricingApplicationTests.java
+++ src/test/java/com/capitole/challenge/pricing/PricingApplicationTests.java
@@ -1,13 +1,21 @@
 package com.capitole.challenge.pricing;
 
+import com.capitole.challenge.pricing.controller.PriceController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PricingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private PriceController controller;
+
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
 
 }
