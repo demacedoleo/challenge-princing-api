324a4eba918c6a72e661af1b35a3ff6549aaba8a
demacedoleo <leonardo.demacedo@pedidosya.com>
2022-12-13 14:46:54 -0300

    define interface for service layer

--- src/main/java/com/capitole/challenge/pricing/service/PriceService.java
+++ src/main/java/com/capitole/challenge/pricing/service/PriceService.java
@@ -1,4 +1,10 @@
 package com.capitole.challenge.pricing.service;
 
+import com.capitole.challenge.pricing.domain.Price;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface PriceService {
+    Optional<Price> findCurrentProductPrice(int brandID, int productID, LocalDateTime date, Boolean enabled) throws Exception;
 }
--- src/main/java/com/capitole/challenge/pricing/service/PriceServiceImpl.java
+++ src/main/java/com/capitole/challenge/pricing/service/PriceServiceImpl.java
@@ -1,4 +1,13 @@
 package com.capitole.challenge.pricing.service;
 
+import com.capitole.challenge.pricing.domain.Price;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public class PriceServiceImpl implements PriceService {
+    @Override
+    public Optional<Price> findCurrentProductPrice(int brandID, int productID, LocalDateTime date, Boolean enabled) {
+        return Optional.empty();
+    }
 }
