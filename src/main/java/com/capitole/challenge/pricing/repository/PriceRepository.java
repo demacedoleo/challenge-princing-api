4e6906d9d88ca423526007f819343537cc248e41
demacedoleo <leonardo.demacedo@pedidosya.com>
2022-12-12 23:21:43 -0300

    data access custom query to find current product price

--- src/main/java/com/capitole/challenge/pricing/repository/PriceRepository.java
+++ src/main/java/com/capitole/challenge/pricing/repository/PriceRepository.java
@@ -1,6 +1,19 @@
 package com.capitole.challenge.pricing.repository;
 
+import com.capitole.challenge.pricing.domain.Price;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface PriceRepository extends CrudRepository<PriceEntity, Integer> {
+    @Query(
+            value = "SELECT p.brandID, p.productID, p.priceList, p.price, p.startDate, p.endDate " +
+                    "FROM prices p " +
+                    "WHERE  p.brandID = :brandID and p.productID = :productID and p.startDate <= :date and " +
+                    "p.enabled = :enabled and p.endDate >= :date",
+            nativeQuery = true
+    )
+    Optional<Price> findCurrentPrice(int brandID, int productID, LocalDateTime date, Boolean enabled);
 }
